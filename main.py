767dcbb89d4f4c84ee9015339558ca6e72cfa7af
pdoazan <paul.doazan@gmail.com>
2025-01-07 14:32:20 +0100

    Achieve phase 1

--- main.py
+++ main.py
@@ -1,7 +1,54 @@
 import requests
+from bs4 import BeautifulSoup
+import csv
+import os
 
-url = "http://books.toscrape.com/"
-page = requests.get(url)
 
-# Voir le code html source
-print(page.content)
+def getContentNextToTableHead(thParameter):
+    try:
+        name_header = soup.find('th', string=thParameter)
+        return name_header.find_next_sibling('td').text if name_header else ''
+    except AttributeError:
+        return ''
+
+
+def getReviewRating(soup):
+    try:
+        star_div = soup.find('div', class_='star-rating')
+        if star_div and star_div.get('class'):
+            return star_div.get('class')[1]
+        return ''
+    except (AttributeError, IndexError):
+        return ''
+
+
+url = "http://books.toscrape.com/catalogue/sharp-objects_997/index.html"
+response = requests.get(url)
+page = response.content
+soup = BeautifulSoup(page, "html.parser")
+
+book_data = {
+    'Title': soup.find("h1").text,
+    'Product Page URL': url,
+    'Universal Product Code': getContentNextToTableHead('UPC'),
+    'Price Including Tax': getContentNextToTableHead('Price (incl. tax)'),
+    'Price Excluding Tax': getContentNextToTableHead('Price (excl. tax)'),
+    'Number Available': getContentNextToTableHead('Availability'),
+    'Product Description': soup.find('div', id='product_description').find_next('p').text,
+    'Category': getContentNextToTableHead('Product Type'),
+    'Review Rating': getReviewRating(soup),
+    'Image URL': soup.find('div', class_='item active').find('img')['src']
+}
+
+print(book_data['Price Including Tax'])
+
+# Write to CSV
+result_dir = 'result'
+filename = os.path.join(result_dir, 'book_data.csv')
+
+with open(filename, 'w', newline='', encoding='utf-8') as f:
+    writer = csv.DictWriter(f, fieldnames=book_data.keys())
+    writer.writeheader()  # Write the headers
+    writer.writerow(book_data)  # Write the data
+
+print(f"Data has been written to {filename}")
